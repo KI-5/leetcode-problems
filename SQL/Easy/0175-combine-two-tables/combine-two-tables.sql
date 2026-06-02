2f855e5305637e3a24d17de86f6c5fdb3a9c632b
KI-5 <ishani.kanapathipillai@gmail.com>
2026-06-02 22:21:58 +0530

    Update combine-two-tables.sql

--- SQL/Easy/0175-combine-two-tables/combine-two-tables.sql
+++ SQL/Easy/0175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 select  Person.firstName, Person.lastName, Address.city, Address.state
 from Person
-left join Address on Person.personId= Address.personId
+left join Address on Person.personId= Address.personId 
